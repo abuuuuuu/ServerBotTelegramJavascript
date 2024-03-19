300ad6b45b538abc169c93cf7cfaeb975821cd34
abuuuuuu <60000040+abuuuuuu@users.noreply.github.com>
2024-03-19 12:00:03 +0100

    Càrrega inicial

---
+++ telegram.js
@@ -0,0 +1,204 @@
+var updateid=0;
+var datatable;
+var dataFile;
+var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function() {
+        var text = reader.result;
+        var node = $('#output');
+        dataFile=JSON.parse(text);
+        var template = Handlebars.compile($('#tmplConfig').html());
+        node.html(template(dataFile.config));//JSON.parse(text).data.prop1;
+        $('div.telegramstop').removeClass('telegramstop').addClass('telegramstart');
+        UpdateTelegram();
+    };
+    reader.readAsText(input.files[0]);
+};
+var openFileData = function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function() {
+        datatable.fnClearTable();
+        datatable.fnAddData(JSON.parse(reader.result));
+    };
+    reader.readAsText(input.files[0]);
+};
+function UpdateTelegram(){
+    //{"ok":true,"result":[{"update_id":820496628,
+        //"message":{
+            //"message_id":798,"from":{
+                //"id":50654474,"is_bot":false,"first_name":"Josep","last_name":"B V","username":"Abuuuuu","language_code":"ca"
+            //},
+            //"chat":{"id":50654474,"first_name":"Josep","last_name":"B V","username":"Abuuuuu",
+                //"type":"private"},
+            //"date":1710190263,
+            //"text":"Prova Text 1"}}]}
+    setTimeout(function(){
+        GetMessages();
+        UpdateTelegram();
+    },1000*$('#segonsupdatelegram').val());
+}
+function SaveFile(filename,text){
+    // Create element with <a> tag
+    var link = document.createElement("a");
+
+    // Create a blog object with the file content which you want to add to the file
+    var file = new Blob([text], { type: 'text/plain' });
+
+    // Add file content in the object URL
+    link.href = URL.createObjectURL(file);
+
+    // Add file name
+    link.download = filename;
+
+    // Add click event to <a> tag to save file.
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+function SendMessage(chatid,message){
+    $.get("https://api.telegram.org/bot"+$('#telegramtoken').val()+"/sendMessage?parse_mode=html&chat_id="+chatid+"&text="+message,function(data){
+        //{"ok":true,"result":{
+            //"message_id":806,
+            //"from":{
+                //"id":6144449443,"is_bot":true,"first_name":"Boda Carol & Josep 2023",
+                //"username":"BodaCarolJosep2023Bot"},
+            //"chat":{"id":50654474,"first_name":"Josep","last_name":"B V","username":"Abuuuuu",
+                //"type":"private"},
+            //"date":1710192902,
+            //"text":"Prova Text 7"}}
+    });
+}
+function GetResposta(message){
+    if (dataFile.preguntes.length>0){
+        var res='';
+        $.each(dataFile.preguntes,function(){
+            if (typeof this.pregunta=='string'){
+                if (this.pregunta==message){
+                    res=this.resposta;
+                    return false;
+                }    
+            }else{
+                var tempresp=this.resposta;
+                $.each(this.pregunta,function(){
+                    if (this==message){
+                        res=tempresp;
+                        return false;
+                    }    
+                });
+            }
+            if (res!='') return false;
+        });
+        if (res!=''){
+            return res
+        }
+        $.each(dataFile.paraules,function(){
+            if (typeof this.paraula=='string'){
+                if (message.indexOf(this.paraula)>=0){
+                    res=this.resposta;
+                    return false;
+                }
+            }else{
+                var tempresp=this.resposta;
+                $.each(this.paraula,function(){
+                    if (message.indexOf(this)>=0){
+                        res=tempresp;
+                        return false;
+                    }    
+                });
+            }
+            if (res!='') return false;
+        });
+        if (res!=''){
+            return res
+        }
+        if ($('#notificarpreguntes').is(':checked')==true){
+            var chatidnotif=$('#chatidnotificar').val();
+            if (chatidnotif.length>0){
+                SendMessage(chatidnotif,'Missatge: '+message);
+            }
+        }
+        return dataFile.preguntesincorrectes[parseInt(Math.random() * dataFile.preguntesincorrectes.length)];
+    }else{
+        return message;
+    }
+}
+function GetDateTelegram(value){
+    return (new Date(value*1000)).toLocaleString('en-GB');
+}
+function GetMessages(){
+    $.get("https://api.telegram.org/bot"+$('#telegramtoken').val()+"/getUpdates?offset="+updateid,function(data){
+        if (data.result.length>0){
+            $.each(data.result,function(){
+                updateid=this.update_id+1;
+                var nom='';
+                var alies=''
+                if (this.message && this.message.chat){
+                    //if (this.message.chat.first_name.lenght>0){
+                    //    alert(1);
+                    if (this.message.chat.first_name && this.message.chat.first_name.length>0){
+                        nom=nom+' '+this.message.chat.first_name;
+                    }
+                    //}
+                    //if (this.message.chat.last_name.lenght>0){
+                    //    alert(2);
+                    if (this.message.chat.last_name && this.message.chat.last_name.length>0){
+                        nom=nom+' '+this.message.chat.last_name;
+                    }
+                    //}
+                    if (this.message.chat.username && this.message.chat.username.length>0){
+                        alies=this.message.chat.username;
+                    }
+                }
+                var text='';
+                if (this.message && this.message.text && this.message.text.length>0){
+                    text=this.message.text;
+                }
+                try{
+                    datatable.fnAddData([GetDateTelegram(this.message.date),nom,alies,text]);
+                }catch(e){
+                    datatable.fnAddData(['','','',e.message]);
+                }
+                if (dataFile.comandes.saberchatid && dataFile.comandes.saberchatid==text){
+                    SendMessage(this.message.chat.id,this.message.chat.id.toString());
+                }else{
+                    if (dataFile.comandes.modificartemps && dataFile.comandes.modificartemps.length<text.length &&
+                        text.substring(0,dataFile.comandes.modificartemps.length)==dataFile.comandes.modificartemps){
+                        $('#segonsupdatelegram').val(text.substring(dataFile.comandes.modificartemps.length));
+                        SendMessage(this.message.chat.id,"S'ha modificat els temps d'actualització");
+                    }else{
+                        SendMessage(this.message.chat.id,GetResposta(text));
+                    }
+                }
+            });
+        }
+    });
+}
+$(document).ready(function(){
+    $('div.telegramstop').click(function(){
+        $('input#uploadconfig[type="file"]').click();
+    });
+    $('button').button();
+    $('button#Guardar').click(function(){
+        SaveFile((new Date().toISOString().split('.')[0].replace(/[^\d]/gi,'')).toString()+'.txt',JSON.stringify(datatable.fnGetData()));
+    });
+    $('button#Carregar').click(function(){
+        $('input#uploaddata[type="file"]').click();
+    });
+    datatable=$('#example').dataTable({
+        "sScrollY": document.body.offsetHeight-215+"px",
+        "bPaginate": false,
+        "bScrollCollapse": true,
+        "aaSorting": [[ 0, "desc" ]],
+        "oLanguage": {
+            "sLengthMenu": "Mostrar _MENU_ missatges per pàgina",
+            "sZeroRecords": "No hi ha informació",
+            "sInfo": "Mostrar _START_ a _END_ de _TOTAL_ missatges",
+            "sInfoEmpty": "Mostrar 0 a 0 de 0 missatges",
+            "sInfoFiltered": "(filtrar de _MAX_ missatges totals)"
+        },
+        "bJQueryUI": true,
+        "sPaginationType": "full_numbers"
+    });
+});
